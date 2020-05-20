8197cbf28cc10d5afbfc722f74fa46fdc733f155
nebur-01 <garciarub971@gmail.com>
2020-05-20 17:18:18 -0500

    Red

---
+++ 36.Sistema1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections;
+
+namespace _36.Sistema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Procesamos los argumentos que pasamos al programa
+            if( args.Length < 1 ) Menu();
+            else {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                switch(int.Parse(args[0])) {
+                    case 1: InfoSistema(); break;
+                    case 2: Procesos(); break;
+                    case 3: ProcesoActual(); break;
+                    case 4: HilosyModulos(int.Parse(args[1])); break;
+                    default: Console.WriteLine("Opción Invalida ..."); break;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        static void Menu() {
+            //Opciones
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[-] Menú: Procesos, Hilos, Modulos \n");
+            Console.WriteLine("[-] Información del Sistema ....................... [1]");
+            Console.WriteLine("[-] Visualizar procesos del sistema ............... [2]");
+            Console.WriteLine("[-] Visualizar el proceso actual .................. [3]");
+            Console.WriteLine("[-] Ver Hilos y Modulos de un proceso dado ........ [4]");
+            Console.ForegroundColor = ConsoleColor.Black;
+
+        }
+
+        static void InfoSistema() {
+            //Obtener informacion del sistema
+            Console.WriteLine($"Numero de Procesadores : {Environment.ProcessorCount}");
+            Console.WriteLine($"Usuario Actual         : {Environment.UserName}");
+            Console.WriteLine($"Dot Net.Core Versión   : {Environment.Version}");
+            Console.WriteLine($"Versión de SO          : {Environment.OSVersion}");
+            Console.WriteLine($"Nombre de la Maquina   : {Environment.MachineName}");
+            Console.WriteLine($"Sistema es 64bits      : {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"Directorio Actual      : {Environment.CurrentDirectory}");
+            Console.WriteLine($"Unidades Logicas       : {Environment.GetLogicalDrives().Count()}");
+            foreach(string drv in Environment.GetLogicalDrives()) Console.Write($"{drv}  ");  //para desplegar
+            Console.WriteLine($"\n Variables de entorno  : {Environment.GetEnvironmentVariables().Count}");
+            foreach(DictionaryEntry var in Environment.GetEnvironmentVariables()) Console.Write($"{var.Key,-35} = {var.Value}\n");
+        }
+
+        static void Procesos() {
+            //Obtener y listar todos los procesos del sistema
+            var procesos = (from proc in Process.GetProcesses() orderby proc.ProcessName select proc).ToList();
+            Console.WriteLine($"Procesos del sistema : {procesos.Count}");
+            procesos.ForEach(p => Console.WriteLine($"{p.Id} {p.ProcessName}"));
+            Console.WriteLine();
+        }
+
+        static void ProcesoActual() {
+            //Obtener y listar el proceso actual
+            var pa = Process.GetCurrentProcess();
+            Console.WriteLine("El proceso que se ejecuta actualmente :");
+            Console.WriteLine($"{pa.Id} {pa.ProcessName} {pa.StartTime}\n");
+        }
+
+        static void HilosyModulos(int proceso) 
+        {
+            //obtener los Threads y Modulos de un proceso
+            //dependera de los procesos listados en tu sistema en particular
+            try
+            {
+
+                //Obtener proceso por ID
+                Process pr = Process.GetProcessById(proceso);
+                Console.WriteLine("\nInformación de un proceso:");
+                Console.WriteLine($"{pr.Id} {pr.ProcessName}");
+
+                //Obtener hilos del proceso en cuestión 
+                ProcessThreadCollection hilos = pr.Threads;
+                Console.WriteLine($"\n>> Hilos en este proceso:{hilos.Count}");
+                foreach(ProcessThread hilo in hilos) {   // listar
+                    Console.WriteLine($"{hilo.Id} {hilo.ThreadState} {hilo.StartTime} {hilo.PriorityLevel}");
+                }
+
+                //modulos 
+                ProcessModuleCollection modulos = pr.Modules;
+                Console.WriteLine("\n>> Modulos del proceso:");
+                foreach(ProcessModule mod in modulos) {
+                    Console.WriteLine($"{mod.ModuleName}");
+                }
+
+            }
+        
+            catch (ArgumentException err)  // no se desborde el programa
+            {
+            Console.WriteLine(err.Message);
+            }
+
+         }
+    }
+}
---
+++ 37.Sistema2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Diagnostics;
+
+
+namespace _37.Sistema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Procesamos argumentos del usuario 
+            if( args.Length < 1 ) Menu();
+            else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                switch(int.Parse(args[0])) {
+                    case 1: Ping("www.uaz.edu.mx"); break;
+                    case 2: DnsAndIPLocal(); break;
+                    case 3: InterfacesRed(); break;
+                    default: Console.WriteLine("Opción Invalida ..."); break;
+        }
+        Console.ForegroundColor = ConsoleColor.Black;
+      }
+    }
+
+    static void Menu() {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[-] Menú de opciones, operaciones de red \n");
+            Console.WriteLine("[-] Hacer ping a uaz.edu.mx ....................... [1]");
+            Console.WriteLine("[-] Obtener nombre host e ip de mi PC ............. [2]");
+            Console.WriteLine("[-] Obtener interfaces de red de mi PC ............ [3]");
+            Console.ForegroundColor = ConsoleColor.Black;
+    }
+
+//
+    static void Ping(string ipaddr) {  
+        Ping ping = new Ping();
+        Console.WriteLine("Haciendo ping a uaz.edu.mx");
+        PingReply reply = ping.Send(ipaddr,1000); //analiza la respuesta 
+        Console.WriteLine($"Respuesta: {reply.Status.ToString()}");
+    }
+
+    static void DnsAndIPLocal() {
+        string hostname = Dns.GetHostName();
+        string ip = Dns.GetHostEntry(hostname).AddressList[0].ToString();
+        Console.WriteLine($"Nombre del host : {hostname}");
+        Console.WriteLine($"Ip del host     : {ip}");
+    }
+
+    static void InterfacesRed()
+    {
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces(); //
+        Console.WriteLine($"Interfaces Wireless o Ethernet:");
+        foreach(NetworkInterface interfaz in interfaces)  //arreglo NetworkInterface
+        {
+            if(interfaz.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                interfaz.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    Console.Write($"{interfaz.Id.ToString(),-8}");
+                    Console.Write($"{interfaz.NetworkInterfaceType.ToString(),-10}");
+                    Console.Write($"{interfaz.GetPhysicalAddress().ToString(),-13}");
+                    UnicastIPAddressInformationCollection uniIps = interfaz.GetIPProperties().UnicastAddresses;
+                    foreach(UnicastIPAddressInformation ip in uniIps)
+                        Console.Write($"{ip.Address.ToString(),-30}");
+                    Console.WriteLine();
+                }
+
+        }
+    }
+  }
+}
