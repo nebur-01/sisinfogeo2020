c44d79eaa911c248cfe07b5d8935b1faab7fbbee
nebur-01 <garciarub971@gmail.com>
2020-05-28 16:49:11 -0500

    AppFc

--- 38.FacebookAp1/Program.cs
+++ 38.FacebookAp1/Program.cs
@@ -9,7 +9,7 @@ namespace _38.FacebookAp1
         {
 
             //token
-            string fbtoken  = "EAACyGR7aItABAGxxKQoQZASqV1NrCrmRbkYh1UsMTcrQqzQdlIZBtAPPMrsfZCxQ3YUwGDyNo08p2H6ZCWJq9CRM7YkF9CSFmaAirmdbpzhbZBp68IPLcUV1Mlvh8aCdMlw6nlcWPTfdjctRbuEStjCG36V4snM8tjcMLIfv3aoFA4cpjni5uc6bQGvF7l2UZD";
+            string fbtoken  = "EAACyGR7aItABAHp2ZAphjx04BMfZB5pQjTtcuuwdTOu45k8DO4vQpMgdLm4ZA2WR1T75xCywPU8waf3NQ6F0Ql4vo1U3kLdpis1xN6jbphm7Tw61PvzK1ZBbOhs0tZAlECPypIeabt4V5sryvwheazZBb0v4J49ckCjBRC7OxxJt6CIsv7WsE1TkHHqEkp6J8ZD";
             string fbfields = "id,first_name,last_name,middle_name,name,name_format,picture,short_name,email";
 
             FacebookApi fb = new FacebookApi(fbtoken);
